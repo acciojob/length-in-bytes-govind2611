ba7cc373b98771a7cd109d713189e9d936b9c64c
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-24 09:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const byteSize = (str) => {
-  // write your code here
+let byteSize = 0;
+for (let i = 0; i < str.length; i++) {
+let charCode = str.charCodeAt(i);
+// UTF-16 encoding for characters in range of 0-127 (ASCII characters)
+if (charCode <= 127) {
+byteSize += 1;
+} else {
+// UTF-8 encoding for characters outside ASCII range
+byteSize += encodeURIComponent(str[i]).replace(/%../g, "x").length;
+}
+}
+return byteSize;
 };
 
 // Do not change the code below
